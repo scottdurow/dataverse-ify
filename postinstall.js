e8f6bb33a1cc74d01d8f3f359006597ad33dda3f
scottdurow <scott_durow@hotmail.com>
2022-11-21 16:18:12 -0800

    added version 2 message

---
+++ postinstall.js
@@ -0,0 +1,3 @@
+console.log(`\n🙌 Thank you for using dataverse-ify version 1.🙌
+Please consider upgrading to version 2.
+There are some minor breaking changes. Read more at https://github.com/scottdurow/dataverse-ify`);
